678034356d5327270847bad53dc52359e5714b4f
rcviana <rcviana@gmail.com>
2013-10-06 00:23:44 -0500

    First commit - Java restful app example...
    by Robson Viana

---
+++ src/main/java/com/rcviana/rest/status/Status.java
@@ -0,0 +1,29 @@
+package com.rcviana.rest.status;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * @author Robson Viana
+ *
+ */
+@Path("/v1/status/*")
+public class Status {
+
+	private static final String VERSION = "0.1";
+
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String getStatus() {
+		return "<h1>Java Web Service is up ...</h1>";
+	}
+
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	@Path("/version")
+	public String getVersion() {
+		return "<h1>Version : " + VERSION + "</h1>";
+	}
+}
